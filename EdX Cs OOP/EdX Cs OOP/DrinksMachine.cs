467f5b312ba176f71c41692a4709f1e0fe418b0d
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-05-17 20:17:34 -0400

    Module 1 Edx Course

---
+++ EdX Cs OOP/EdX Cs OOP/DrinksMachine.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EdX_Cs_OOP
+{
+    class DrinksMachine
+    {
+        //Auto-implemented member variables
+        private int _age;
+        public int Age { get => _age; set => _age = value; }
+
+        private string _make;
+        public string Make
+        {
+            get { return _make; }
+            set { _make = value; }
+        }
+
+        private string _model;
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        private string _location;
+        public string Location
+        {
+            get { return _location; }
+            set { _location = value; }
+        }
+
+        
+
+        //default constructor
+        public DrinksMachine()
+        {
+            Age = 0;
+        }
+        //Constructors
+        public DrinksMachine(int age)
+        {
+            this.Age = age;
+        }
+        public DrinksMachine(string make, string model)
+        {
+            this.Make = make;
+            this.Model = model;
+        }
+        public DrinksMachine(int age, string make, string model)
+        {
+            this.Age = age;
+            this.Make = make;
+            this.Model = model;
+        }
+        public void MakeCappuccino()
+        {
+            //method for cappos go here, dood
+            Console.WriteLine("I made you a cappo, dood.");
+        }
+        public void MakeEspresso()
+        {
+            //its pronounced "espresso" not "expresso", dipshit
+        }
+    }
+}
---
+++ EdX Cs OOP/EdX Cs OOP/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EdX_Cs_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            DrinksMachine dm = new DrinksMachine("Folgers", "Caffo Blasto");
+            dm.Location = "Kitchen";
+            dm.MakeCappuccino();
+
+            Console.WriteLine(dm.Location);
+
+        }
+    }
+}
