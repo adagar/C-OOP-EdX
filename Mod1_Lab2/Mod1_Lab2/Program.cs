5190c3cf27017090e8b9ed9911189aef2f38fc94
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-05-19 11:08:27 -0400

    Complete Mod1_Lab1

---
+++ Mod1_Lab2/Mod1_Lab2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mod1_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
