fc3ed7654294d2c02d71ca694c5823947f130ca0
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-05-28 14:24:14 -0400

    Finished mod 3

---
+++ Mod3_Lab1/Mod3_Lab1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace Mod3_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create StreamReader  and assign to null
+            StreamReader streamReaderObject = null;
+
+            try
+            {
+                streamReaderObject = new StreamReader("file1.txt");
+
+                String contents = streamReaderObject.ReadToEnd();
+
+                streamReaderObject.Close();
+
+                Console.WriteLine("The file has {0} text elements.", new StringInfo(contents).LengthInTextElements);
+            }
+            catch(FileNotFoundException)
+            {
+                Console.WriteLine("The file cannot be found.");
+            }
+        }
+    }
+}
